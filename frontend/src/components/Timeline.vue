c0fff0e388153b0038e20de2a3fc394d58215d1c
DEROGUERRE GUILLAUME p1614920 <p1614920@univ-lyon1.fr>
2017-06-23 08:19:15 +0200

    list tweet

---
+++ frontend/src/components/Timeline.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="timeline">
+    <h1>{{ msg }}</h1>
+    <router-link to="/">
+      Accueil
+    </router-link>
+    <ul>
+      <li v-for="tweet in tweets">{{ tweet.auteur }} say: {{ tweet.contenu }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'timeline',
+  data () {
+    return {
+      tweets: [
+        {
+          'auteur': 'Bingo',
+          'contenu': 'salut !'
+        },
+        {
+          'auteur': 'MrJoe',
+          'contenu': 'hi'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+ list-style-type: none;
+ padding: 0;
+}
+li {
+ display: block;
+ margin: 0 10px;
+}
+</style>
